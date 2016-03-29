edc34b0cc27a1f32964887e7846571379cf8a623
greg <gregc.sg@googlemail.com>
2016-03-29 20:03:36 +0100

    Add a FaireBot Forward block
    - currently under tinkerkit menu

---
+++ src/main/java/com/ardublock/translator/block/FaireBot/FaireBotForwardBlock.java
@@ -0,0 +1,50 @@
+package com.ardublock.translator.block.FaireBot;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.ServoBlock;
+import com.ardublock.translator.block.NumberBlock;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+import com.ardublock.translator.block.exception.BlockException;
+
+import com.ardublock.translator.block.tinker.TinkerOutputPortBlock;
+
+public class FaireBotForwardBlock extends TranslatorBlock
+{
+	public FaireBotForwardBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		if (!(translatorBlock instanceof NumberBlock))
+		{
+			throw new BlockException(this.blockId, "the rotation of FaireBotForward must a number");
+		}
+		
+		
+		NumberBlock rotationNumberBlock = (NumberBlock)translatorBlock;
+		String rotationNumber = rotationNumberBlock.toCode();
+		//String servoName = "servo_pin_" + pinNumber;
+		
+		//translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+		
+		String ret = "digitalWrite(8, HIGH);\n";
+        ret = ret + "for (int i = 0; i < " + rotationNumber + "; i++)\n{"; 
+		ret = ret + "  leftServo.write(180);\n  rightServo.write(0);\n  delay(30);\n}";
+        ret = ret + "digitalWrite(8, LOW);\nleftServo.write(90);\nrightServo.write(90);\ndelay(30);";
+		
+		//servoName + ".write( " + translatorBlock.toCode() + " );\n";
+		translator.addHeaderFile("Servo.h");
+		translator.addDefinitionCommand("Servo leftServo;");
+		translator.addDefinitionCommand("Servo rightServo;");
+		translator.addSetupCommand("leftServo.attach(A1);");
+		translator.addSetupCommand("rightServo.attach(A2);");
+		return ret;
+	}
+
+}
